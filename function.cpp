f29874633d32f8adec9578c5567ae74ab6643ea8
emmanuelham <112572553+emmanuelham@users.noreply.github.com>
2022-09-12 11:16:26 -0500

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+using namespace std;
+
+// Write a function that will take as input an integer and will return
+// double that value
+// If there is no input value, then input should be -1
+int doubleNum(int inNum) {
+return (2*inNum);
+}
+
+int main(){
+cout << doubleNum(3) << endl;}
