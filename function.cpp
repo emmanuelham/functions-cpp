a3c4c469f58f4dd0f212c3a6ddf09696637391db
emmanuelham <112572553+emmanuelham@users.noreply.github.com>
2022-09-12 11:41:49 -0500

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -25,9 +25,16 @@ return (a+b+c)}
 // Write a function that will take in an array and size as input
 // the function should return nothing
 // the function should print the array.
-
+void printChars(char myArr[], int size){
+  for(int index = 0;int < size; index + 1){
+    cout << myArr[index] << endl;}
+  cout << endl;
+}
 
 
 int main() {
   cout << doubleNum() << endl;
-  cout << sumFloats(0.5, 0.9) << endl;}
+  cout << sumFloats(0.5, 0.9) << endl;
+  const int SIZE = 5;
+  char myChars[5] = {'h', 'e', 'l', 'l', 'o'};
+  printChars(myChars, 5);}
