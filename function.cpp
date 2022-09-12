74a4e97580c12b33d0f60d0a7381b268b53b45ea
emmanuelham <112572553+emmanuelham@users.noreply.github.com>
2022-09-12 11:31:42 -0500

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -5,9 +5,29 @@ using namespace std;
 // Write a function that will take as input an integer and will return
 // double that value
 // If there is no input value, then input should be -1
-int doubleNum(int inNum) {
+int doubleNum(int inNum = -1) {
 return (2*inNum);
 }
 
 int main(){
 cout << doubleNum(3) << endl;}
+
+
+// Write a function that will take inputs of floats a, b and c
+// a is required 
+// if b has no input then value for b is 0.1
+// c is required
+// output: the sum of a, b and c
+
+float sumFloats(float a, float b = 0.1, float c){
+return (a+b+c)}
+
+// Write a function that will take in an array and size as input
+// the function should return nothing
+// the function should print the array.
+
+
+
+int main() {
+  cout << doubleNum() << endl;
+  cout << sumFloats(0.5, 0.9) << endl;}
